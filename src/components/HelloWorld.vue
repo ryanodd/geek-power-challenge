8b8a6bb345394f49ad4f32ea841b49f96e345276
Ryan Odd <ryanodd@gmail.com>
2021-02-10 21:41:27 -0500

    Vue hello world

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    {{'ayyyyyy'}}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.class{
+  
+}
+</style>
